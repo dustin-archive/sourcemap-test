01b7e53ced4209b9b87bf3b6337cecf779ab7738
Dustin Dowell <dustindowell22@gmail.com>
2018-03-29 16:14:21 -0500

    babel-cli

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,2 @@
-!function(){"use strict";var r=function(r){return"foo "+r};alert(r("bar")),alert(r("bar")),alert("foo"+" bar")}();
+"use strict";!function(){var r=function(r){return"foo "+r};alert(r("bar")),alert(r("bar")),alert("foo"+" bar")}();
 //# sourceMappingURL=app.js.map
