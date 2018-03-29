ddf10ec7c0d1bfc8f5e1055e723294e3a37efb83
Dustin Dowell <dustindowell22@gmail.com>
2018-03-29 14:55:35 -0500

    make sync

--- scripts/babel.js
+++ scripts/babel.js
@@ -1,34 +1,24 @@
 
-const { readFile, writeFile } = require('fs')
-const { transformFile } = require('@babel/core')
+const fs = require('fs')
+const babel = require('@babel/core')
 
-readFile('dist/app.js.map', 'utf8', (err, sourceMap) => {
-  if (err) throw err
-
-  const options = {
-    inputSourceMap: JSON.parse(sourceMap),
-    plugins: [
-      require('@babel/plugin-transform-arrow-functions'),
-      require('@babel/plugin-transform-block-scoped-functions'),
-      require('@babel/plugin-transform-block-scoping'),
-      require('@babel/plugin-transform-computed-properties'),
-      require('@babel/plugin-transform-destructuring'),
-      require('@babel/plugin-transform-for-of'),
-      require('@babel/plugin-transform-function-name'),
-      require('@babel/plugin-transform-parameters'),
-      require('@babel/plugin-transform-shorthand-properties'),
-      require('@babel/plugin-transform-spread'),
-      require('@babel/plugin-transform-template-literals')
-    ]
-  }
-
-  transformFile('dist/app.js', options, (err, result) => {
-    if (err) throw err
-
-    process.stdout.write(result.code)
-
-    writeFile('dist/app.js.map', JSON.stringify(result.map), 'utf8', err => {
-      if (err) throw err
-    })
-  })
+const result = babel.transformFileSync('dist/app.js', {
+  inputSourceMap: JSON.parse(fs.readFileSync('dist/app.js.map', 'utf8')),
+  plugins: [
+    require('@babel/plugin-transform-arrow-functions'),
+    require('@babel/plugin-transform-block-scoped-functions'),
+    require('@babel/plugin-transform-block-scoping'),
+    require('@babel/plugin-transform-computed-properties'),
+    require('@babel/plugin-transform-destructuring'),
+    require('@babel/plugin-transform-for-of'),
+    require('@babel/plugin-transform-function-name'),
+    require('@babel/plugin-transform-parameters'),
+    require('@babel/plugin-transform-shorthand-properties'),
+    require('@babel/plugin-transform-spread'),
+    require('@babel/plugin-transform-template-literals')
+  ]
 })
+
+process.stdout.write(result.code)
+
+fs.writeFileSync('dist/app.js.map', JSON.stringify(result.map), 'utf8')
